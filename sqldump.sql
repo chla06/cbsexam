6806b21e103a19813c0b1394bb2d23f30f1786d4
chla06 <45968564+chla06@users.noreply.github.com>
2018-12-19 11:23:16 +0100

    Update sqldump.sql

--- sqldump.sql
+++ sqldump.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `address`
 --
-
+USE cbsexam;
 DROP TABLE IF EXISTS `address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
